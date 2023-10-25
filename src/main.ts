d0b275dc3599d75ce5388b5a3c6e943590853c92
Quentin Hasnaoui Moreau <68105158+qm-h@users.noreply.github.com>
2023-10-25 10:24:06 +0200

    Change api port

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import createFootprintRouter from "./routes/footprint.route";
 import createFoodPrintRouter from "./routes/foodprint.route";
 
 const app = express();
-const port = 3000;
+const port = 9000;
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use('/footprint', createFootprintRouter);
-app.use('/foodprint', createFoodPrintRouter);
+app.use('/api/footprint', createFootprintRouter);
+app.use('/api/foodprint', createFoodPrintRouter);
 
 app.listen(port, () => {
-    console.log('Example app listening on port 3000!');
+    console.log(`Server started at http://localhost:${port}`);
 })
